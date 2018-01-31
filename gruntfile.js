17c2856939a5487b0db8aec216e295a9cf7afa01
Edmond O'Connell <edmond@tommyjohnwear.com>
2018-01-31 09:53:07 -0500

    Setup grunt for cssmin + ugilfy

--- gruntfile.js
+++ gruntfile.js
@@ -1,12 +1,16 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     watch: {
-      source: {
+      css: {
         files: ["sass/**/*.scss"],
-        tasks: ["sass"], //runs the sass task
+        tasks: ["sass", "postcss", "cssmin"], //runs the sass task
         options: {
           livereload: true // needed to run LiveReload
         }
+      },
+      js: {
+        files: "**/*.js",
+        tasks: ["uglify"]
       }
     },
     sass: {
@@ -18,13 +22,50 @@ module.exports = function(grunt) {
           "public/stylesheets/style.css": "sass/style.scss"
         }
       }
+    },
+    postcss: {
+      // Begin Post CSS Plugin
+      options: {
+        map: false,
+        processors: [
+          require("pixrem")(), // add fallbacks for rem units
+          require("autoprefixer")({ browsers: "last 2 versions" }), // add vendor prefixes
+          require("cssnano")() // minify the result
+        ]
+      },
+      dist: {
+        src: "public/stylesheets/style.css"
+      }
+    },
+    cssmin: {
+      // Begin CSS Minify Plugin
+      target: {
+        files: {
+          "public/stylesheets/style.min.css": "public/stylesheets/style.css"
+        }
+      }
+    },
+    uglify: {
+      // Begin JS Uglify Plugin
+      my_target: {
+        files: {
+          "public/scripts/script.min.js": ["public/scripts/*.js"]
+        }
+      }
+      // build: {
+      //   src: ["src/*.js"],
+      //   dest: "public/scripts/script.min.js"
+      // }
     }
   });
 
   // Load Grunt plugins
-  //grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-sass");
   grunt.loadNpmTasks("grunt-sass");
   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
 
   grunt.registerTask("default", ["watch"]);
 };
---
+++ public/scripts/utils.js
@@ -0,0 +1,10 @@
+console.log("utils connected");
+function doesItContain (arr, item) {
+  if (arr.includes(item)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
