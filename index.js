beba73fa9e92db71cf61978dab07ab0834ce644f
Edmond O'Connell <edmond@tommyjohnwear.com>
2018-02-05 11:19:47 -0500

    changed home to homepage url path

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ app.use(express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
-app.get("/", function(request, response) {
+app.get('/', function(req,res){
+  res.send('please redirect')
+})
+
+app.get("/homepage", function(request, response) {
   response.render("pages/index");
 });
 
