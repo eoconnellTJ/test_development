c74e54d3e817d56dd9005b42ffa38cd0a3fe7bba
Edmond O'Connell <edmond@tommyjohnwear.com>
2018-03-29 12:47:35 -0400

    Added footer to bottom of page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var pg = require("pg");
 var products = require("./data/products");
+var footerlinks = require("./data/footer");
 
 app.set("port", process.env.PORT || 5000);
 
@@ -11,38 +12,24 @@ app.use(express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
-  res.render("pages/default");
-});
-
-app.get("/data.json", function(req, res) {
-  res.send(products).catch(err => {
-    console.log("the error is =", err);
-  });
-});
-
-app.get("/homepage", function(request, response) {
-  response.render("pages/index");
+app.get("/", function(request, response) {
+  response.render("pages/index", { footerlinks });
 });
 
 app.get("/category", (req, res) => {
-  res.render("pages/category");
+  res.render("pages/category", { footerlinks });
 });
 
 app.get("/product", (req, res) => {
-  res.render("pages/product");
+  res.render("pages/product", { products, footerlinks });
 });
 
 app.get("/custom", (req, res) => {
-  res.render("pages/custom");
+  res.render("pages/custom"), { footerlinks };
 });
 
 app.get("/cart", (req, res) => {
-  res.render("pages/cart");
-});
-
-app.get("/cool", function(request, response) {
-  response.send(cool());
+  res.render("pages/cart", { footerlinks });
 });
 
 app.listen(app.get("port"), function() {
